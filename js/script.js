423d53fcc5324dadaf16bc717f25d1a3cabae5dd
AKapran8 <kapranandriyu@gmail.com>
2021-02-21 18:34:24 +0200

    first version finish

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+   $('.header__burger').click(function (event) {
+      $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+});
+
+
+// Плавный скролл
+$(document).ready(function () {
+   $('#btn__me').on('click', function (e) {
+      e.preventDefault();
+      let top = $('#me').offset().top;
+      $('html, body').animate({
+         scrollTop: top
+      }, 500);
+   });
+
+   $('#btn__skills').on('click', function (e) {
+      e.preventDefault();
+      let top = $('#skills').offset().top;
+      $('html, body').animate({
+         scrollTop: top
+      }, 1000);
+   });
+
+   $('#btn__education').on('click', function (e) {
+      e.preventDefault();
+      let top = $('#education').offset().top;
+      $('html, body').animate({
+         scrollTop: top
+      }, 1200);
+   });
+
+   $('#btn__projects').on('click', function (e) {
+      e.preventDefault();
+      let top = $('#projects').offset().top;
+      $('html, body').animate({
+         scrollTop: top
+      }, 1700);
+   });
+});
